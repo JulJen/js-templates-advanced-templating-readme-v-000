06b513c60d00bfa841d20b159a298e340e863f1b
Jul Jen <juliajen.prog@gmail.com>
2018-10-12 05:33:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ function loadIssue() {
     body: "Instructions say GET /team and POST /newteam. Rspec wants GET/newteam and POST/team."
   }
 
+  //"compile" markup and delimiters as part of a function that we can call with our context to output a rendered HTML string in var result = template(issue)
+   //we could call template() again and get the same template rendered with the new data
+
   var template = Handlebars.compile(document.getElementById("issue-template").innerHTML);
   var result = template(issue);
   document.getElementsByTagName("main")[0].innerHTML += result;
